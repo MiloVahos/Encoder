8b6d653cc4325a245c7b3570873c06b66294309f
Camilo Vahos <milovahos@gmail.com>
2019-03-31 14:20:15 -0500

    Archivo de pruebas secuenciales

---
+++ secuencialTests.bash
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# FIXING K
+for B in 10000000 50000000 100000000
+do
+    for L in 256 512 1024
+    do 
+        cp ./L$L\_K26_B$B/GRCh38.align ./
+        echo "ARCHIVO L'$L'_K26_B$B/GRCh38.align COPIADO" >> "secuencialTest.txt"
+        echo "PRUEBAS SECUENCIAL" >> "secuencialTest.txt"
+        ./EncoderSec >> "secuencialTest.txt"
+    done
+done
+
+for B in 10000000 50000000 100000000
+do
+    for L in 256 512 1024
+    do 
+        cp ./L$L\_K128_B$B/GRCh38.align ./
+        echo "ARCHIVO L'$L'_K128_B$B/GRCh38.align COPIADO" >> "secuencialTest.txt"
+        echo "PRUEBAS SECUENCIAL" >> "secuencialTest.txt"
+        ./EncoderSec >> "secuencialTest.txt"
+    done
+done
