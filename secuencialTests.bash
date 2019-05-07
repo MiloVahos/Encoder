c4fd278146d353217ddae6c42f8f9008d0aa04e2
Camilo Vahos <milovahos@gmail.com>
2019-05-07 05:16:17 -0500

    Secuencial test 2

--- secuencialTests.bash
+++ secuencialTests.bash
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-echo "EMPIEZAN PRUEBAS FULL" >> "secuencialTest.txt"
+echo "EMPIEZAN PRUEBAS FULL" >> "secuencialTest2.txt"
 for B in 10000000 50000000 100000000
 do
     cp ./FPL1024\_K26_B$B/GRCh38.align ./
-    echo "ARCHIVO L1024_K26_B$B/GRCh38.align COPIADO" >> "secuencialTest.txt"
-    echo "PRUEBAS SECUENCIAL FUll" >> "secuencialTest.txt"
-    ./EncoderSecFull >> "secuencialTest.txt"
+    echo "ARCHIVO L1024_K26_B$B/GRCh38.align COPIADO" >> "secuencialTest2.txt"
+    echo "PRUEBAS SECUENCIAL FUll" >> "secuencialTest2.txt"
+    ./EncoderSecFull >> "secuencialTest2.txt"
     rm GRCh38.align
     echo "TERMINADO L1024_K26_B$B/"
 done
@@ -14,9 +14,9 @@ done
 for B in 10000000 50000000 100000000
 do
     cp ./FPL1024\_K128_B$B/GRCh38.align ./
-    echo "ARCHIVO L1024_K128_B$B/GRCh38.align COPIADO" >> "secuencialTest.txt"
-    echo "PRUEBAS SECUENCIAL FUll" >> "secuencialTest.txt"
-    ./EncoderSecFull >> "secuencialTest.txt"
+    echo "ARCHIVO L1024_K128_B$B/GRCh38.align COPIADO" >> "secuencialTest2.txt"
+    echo "PRUEBAS SECUENCIAL FUll" >> "secuencialTest2.txt"
+    ./EncoderSecFull >> "secuencialTest2.txt"
     rm GRCh38.align
     echo "TERMINADO L1024_K128_B$B/"
 done
@@ -24,22 +24,22 @@ done
 for B in 10000000 50000000 100000000
 do
     cp ./FPL1024\_K256_B$B/GRCh38.align ./
-    echo "ARCHIVO L1024_K256_B$B/GRCh38.align COPIADO" >> "secuencialTest.txt"
-    echo "PRUEBAS SECUENCIAL FUll" >> "secuencialTest.txt"
-    ./EncoderSecFull >> "secuencialTest.txt"
+    echo "ARCHIVO L1024_K256_B$B/GRCh38.align COPIADO" >> "secuencialTest2.txt"
+    echo "PRUEBAS SECUENCIAL FUll" >> "secuencialTest2.txt"
+    ./EncoderSecFull >> "secuencialTest2.txt"
     rm GRCh38.align
     echo "TERMINADO L1024_K256_B$B/"
 done
 
-echo "FIN PRUEBAS FULL ********************" >> "secuencialTest.txt"
-echo "EMPIEZAN PRUEBAS MID ********************" >> "secuencialTest.txt"
+echo "FIN PRUEBAS FULL ********************" >> "secuencialTest2.txt"
+echo "EMPIEZAN PRUEBAS MID ********************" >> "secuencialTest2.txt"
 
 for B in 10000000 50000000 100000000
 do
     cp ./FPL1024\_K26_B$B/GRCh38.align ./
-    echo "ARCHIVO L1024_K26_B$B/GRCh38.align COPIADO" >> "secuencialTest.txt"
-    echo "PRUEBAS SECUENCIAL MID" >> "secuencialTest.txt"
-    ./EncoderSecMid >> "secuencialTest.txt"
+    echo "ARCHIVO L1024_K26_B$B/GRCh38.align COPIADO" >> "secuencialTest2.txt"
+    echo "PRUEBAS SECUENCIAL MID" >> "secuencialTest2.txt"
+    ./EncoderSecMid >> "secuencialTest2.txt"
     rm GRCh38.align
     echo "TERMINADO L1024_K26_B$B/"
 done
@@ -47,9 +47,9 @@ done
 for B in 10000000 50000000 100000000
 do
     cp ./FPL1024\_K128_B$B/GRCh38.align ./
-    echo "ARCHIVO L1024_K128_B$B/GRCh38.align COPIADO" >> "secuencialTest.txt"
-    echo "PRUEBAS SECUENCIAL MID" >> "secuencialTest.txt"
-    ./EncoderSecMid >> "secuencialTest.txt"
+    echo "ARCHIVO L1024_K128_B$B/GRCh38.align COPIADO" >> "secuencialTest2.txt"
+    echo "PRUEBAS SECUENCIAL MID" >> "secuencialTest2.txt"
+    ./EncoderSecMid >> "secuencialTest2.txt"
     rm GRCh38.align
     echo "TERMINADO L1024_K128_B$B/"
 done
@@ -57,22 +57,22 @@ done
 for B in 10000000 50000000 100000000
 do
     cp ./FPL1024\_K256_B$B/GRCh38.align ./
-    echo "ARCHIVO L1024_K256_B$B/GRCh38.align COPIADO" >> "secuencialTest.txt"
-    echo "PRUEBAS SECUENCIAL MID" >> "secuencialTest.txt"
-    ./EncoderSecMid >> "secuencialTest.txt"
+    echo "ARCHIVO L1024_K256_B$B/GRCh38.align COPIADO" >> "secuencialTest2.txt"
+    echo "PRUEBAS SECUENCIAL MID" >> "secuencialTest2.txt"
+    ./EncoderSecMid >> "secuencialTest2.txt"
     rm GRCh38.align
     echo "TERMINADO L1024_K256_B$B/"
 done
 
-echo "FIN PRUEBAS MID ********************" >> "secuencialTest.txt"
-echo "EMPIEZAN PRUEBAS BIN ********************" >> "secuencialTest.txt"
+echo "FIN PRUEBAS MID ********************" >> "secuencialTest2.txt"
+echo "EMPIEZAN PRUEBAS BIN ********************" >> "secuencialTest2.txt"
 
 for B in 10000000 50000000 100000000
 do
     cp ./FPL1024\_K26_B$B/GRCh38.align ./
-    echo "ARCHIVO L1024_K26_B$B/GRCh38.align COPIADO" >> "secuencialTest.txt"
-    echo "PRUEBAS SECUENCIAL BIN" >> "secuencialTest.txt"
-    ./EncoderSecBin >> "secuencialTest.txt"
+    echo "ARCHIVO L1024_K26_B$B/GRCh38.align COPIADO" >> "secuencialTest2.txt"
+    echo "PRUEBAS SECUENCIAL BIN" >> "secuencialTest2.txt"
+    ./EncoderSecBin >> "secuencialTest2.txt"
     rm GRCh38.align
     echo "TERMINADO L1024_K26_B$B/"
 done
@@ -80,9 +80,9 @@ done
 for B in 10000000 50000000 100000000
 do
     cp ./FPL1024\_K128_B$B/GRCh38.align ./
-    echo "ARCHIVO L1024_K128_B$B/GRCh38.align COPIADO" >> "secuencialTest.txt"
-    echo "PRUEBAS SECUENCIAL BIN" >> "secuencialTest.txt"
-    ./EncoderSecBin >> "secuencialTest.txt"
+    echo "ARCHIVO L1024_K128_B$B/GRCh38.align COPIADO" >> "secuencialTest2.txt"
+    echo "PRUEBAS SECUENCIAL BIN" >> "secuencialTest2.txt"
+    ./EncoderSecBin >> "secuencialTest2.txt"
     rm GRCh38.align
     echo "TERMINADO L1024_K128_B$B/"
 done
@@ -90,11 +90,11 @@ done
 for B in 10000000 50000000 100000000
 do
     cp ./FPL1024\_K256_B$B/GRCh38.align ./
-    echo "ARCHIVO L1024_K256_B$B/GRCh38.align COPIADO" >> "secuencialTest.txt"
-    echo "PRUEBAS SECUENCIAL BIN" >> "secuencialTest.txt"
-    ./EncoderSecBin >> "secuencialTest.txt"
+    echo "ARCHIVO L1024_K256_B$B/GRCh38.align COPIADO" >> "secuencialTest2.txt"
+    echo "PRUEBAS SECUENCIAL BIN" >> "secuencialTest2.txt"
+    ./EncoderSecBin >> "secuencialTest2.txt"
     rm GRCh38.align
     echo "TERMINADO L1024_K256_B$B/"
 done
 
-echo "FIN PRUEBAS BIN ********************" >> "secuencialTest.txt"
+echo "FIN PRUEBAS BIN ********************" >> "secuencialTest2.txt"
